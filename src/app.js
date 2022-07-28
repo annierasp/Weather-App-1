e7fdaf107af37b36aa25a0e6b9834499659174ab
annierasp <annayushchyshyna@gmail.com>
2022-07-29 01:18:08 +0300

    Search engine

---
+++ src/app.js
@@ -0,0 +1,24 @@
+function searchCityValue(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city-name").value;
+  search(city);
+}
+
+let citySearch = document.querySelector("#search-city");
+citySearch.addEventListener("submit", searchCityValue);
+
+function search(city) {
+  let apiKey = "080cbfa2309f6d5006d04e646210a21b";
+  let units = "metric";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(url).then(showTemperature);
+}
+
+function showTemperature(response) {
+  document.querySelector("#current-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("h1").innerHTML = response.data.name;
+}
+
+search("Kyiv");
