d168a7f0f8ceb3c7062bc5f7337f2e4a06e3b18f
annierasp <annayushchyshyna@gmail.com>
2022-07-30 15:35:06 +0300

    Added date js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function searchCityValue(event) {
   event.preventDefault();
   let city = document.querySelector("#search-city-name").value;
@@ -15,12 +38,14 @@ function search(city) {
 }
 
 function showTemperature(response) {
+  let dateElement = document.querySelector("#date");
   document.querySelector("#current-temperature").innerHTML = Math.round(
     response.data.main.temp
   );
   document.querySelector("h1").innerHTML = response.data.name;
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 search("Kyiv");
